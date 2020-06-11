c4d4cfac5614ec610d3dee576b68898bd7e65a65
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-11 16:26:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>patie 9 exercice 8</title>
+</head>
+<body>
+    <?php 
+        $passeDate = date('d-m-Y', strtotime('-22 days'));
+    ?>
+    <p><?= 'Il y a 22 jours nous etions le  ' . $passeDate ?></p>
+</body>
+</html>
